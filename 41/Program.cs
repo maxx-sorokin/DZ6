94320f3670ddc314d250c6416287766659434b12
Maxim Sorokin <maximka773@mail.ru>
2022-11-17 15:24:45 +0300

    41 done

--- 41/Program.cs
+++ 41/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Сколько чисел будете вводить?");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Отлично, теперь можете ввести {m} чисел");
+
+int[] numbers = EnteringArray(m);
+PrintArray(numbers);
+
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbersInArray(numbers)}");
+
+int CountPositiveNumbersInArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] EnteringArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        bool isParsedElement = int.TryParse(Console.ReadLine(), out int element);
+
+        if (!isParsedElement)
+        {
+            Console.WriteLine("Ошибка! Не правильно введены данные, попробуйте ещё раз");
+            i -= 1;
+        }
+        else
+        {
+            array[i] = element;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+    Console.WriteLine();
+}
--- 43/Program.cs
+++ 43/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
